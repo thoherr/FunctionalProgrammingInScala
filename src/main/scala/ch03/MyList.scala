a46554c265b3ca8c97919f4f04657551fbff86be
Thomas Herrmann <mail@thoherr.de>
2016-07-09 20:57:27 +0200

    Exercises 3.7 to 3.13

--- src/main/scala/ch03/MyList.scala
+++ src/main/scala/ch03/MyList.scala
@@ -18,6 +18,10 @@ object MyList {
     case Cons(x, xs) => x * product(xs)
   }
 
+  def apply[A](as: A*): MyList[A] =
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+
   // Exercise 3.2
   def tail[A](ls: MyList[A]): MyList[A] = ls match {
     case Nil => Nil // could be an error
@@ -37,18 +41,63 @@ object MyList {
     else drop(tail(l), n - 1)
 
   // Exercise 3.5
-  def dropWhile[A](l:MyList[A], f: A => Boolean) : MyList[A] = l match {
-    case Nil => Nil  // This could also be an error
-    case Cons(x, xs) => if (f(x)) dropWhile(tail(l), f) else l
+  def dropWhile[A](l: MyList[A])(f: A => Boolean): MyList[A] = l match {
+    case Cons(x, xs) if f(x) => dropWhile(xs)(f)
+    case _ => l
   }
 
   // Exercise 3.6
-  def init[A](l: MyList[A]) : MyList[A] = l match {
+  def init[A](l: MyList[A]): MyList[A] = l match {
     case Cons(x, Nil) => Nil
     case Cons(x, xs) => Cons(x, init(xs))
+    case _ => l
   }
 
-  def apply[A](as: A*): MyList[A] =
-    if (as.isEmpty) Nil
-    else Cons(as.head, apply(as.tail: _*))
+  def foldRight[A, B](as: MyList[A], z: B)(f: (A, B) => B): B = as match {
+    case Nil => z
+    case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+  }
+
+  def sum2(ns: MyList[Int]) =
+    foldRight(ns, 0)((x, y) => x + y)
+
+  def product2(ds: MyList[Double]) =
+    foldRight(ds, 1.0)(_ * _)
+
+  // Exercise 3.7
+  // IMO the only way to stop immediately when encountering 0.0 in product2 would be to introduce another
+  // parameter to foldRight that indicated the end of the recursion (give the null element into the fold)
+
+  // Exercise 3.8
+  val ex38 = foldRight(MyList(1, 2, 3), Nil: MyList[Int])(Cons(_, _))
+  // foldRight exactly matches the structure of the list constructors
+
+  // Exercise 3.9
+  def length[A](as: MyList[A]): Int =
+    foldRight(as, 0)((x, y) => 1 + y)
+
+  // Exercide 3.10
+  def foldLeft[A, B](as: MyList[A], z: B)(f: (B, A) => B): B = {
+    @annotation.tailrec
+    def loop(l: MyList[A], acc: B): B = l match {
+      case Nil => acc
+      case Cons(x, xs) => loop(xs, f(acc, x))
+    }
+    loop(as, z)
+  }
+
+  // Exercise 3.11
+  def sum3(ns: MyList[Int]) =
+    foldLeft(ns, 0)((x, y) => x + y)
+
+  def product3(ds: MyList[Double]) =
+    foldLeft(ds, 1.0)(_ * _)
+
+  def length3[A](as: MyList[A]): Int =
+    foldLeft(as, 0)((x, y) => x + 1)
+
+  // Exercise 3.12
+  def reverse[A](as: MyList[A]): MyList[A] =
+    foldLeft(as, Nil:MyList[A])((xs, x) => Cons(x, xs))
+
 }
