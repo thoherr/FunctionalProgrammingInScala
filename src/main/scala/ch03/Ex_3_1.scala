b12827e4f10ccd6835f05c741521fcd31e416986
Thomas Herrmann <mail@thoherr.de>
2016-07-09 18:27:28 +0200

    Starting with chapter 3

---
+++ src/main/scala/ch03/Ex_3_1.scala
@@ -0,0 +1,15 @@
+package ch03
+
+import MyList._
+
+object Ex_3_1 {
+
+  val l = MyList(1, 2, 3, 4, 5) match {
+    case Cons(x, Cons(2, Cons(4, _))) => x
+    case Nil => 42
+    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+    case Cons(h, t) => h + sum(t)
+    case _ => 101
+  }
+
+}
---
+++ src/main/scala/ch03/MyList.scala
@@ -0,0 +1,30 @@
+package ch03
+
+sealed trait MyList[+A]
+
+case object Nil extends MyList[Nothing]
+
+case class Cons[+A](head: A, tail: MyList[A]) extends MyList[A]
+
+object MyList {
+  def sum(ints: MyList[Int]): Int = ints match {
+    case Nil => 0
+    case Cons(x, xs) => x + sum(xs)
+  }
+
+  def product(ds: MyList[Double]): Double = ds match {
+    case Nil => 1.0
+    case Cons(0.0, _) => 0.0
+    case Cons(x, xs) => x * product(xs)
+  }
+
+  // Exercise 3.2
+  def tail[A](ls: MyList[A]): MyList[A] = ls match {
+    case Nil => Nil // could be an error
+    case Cons(x, xs) => xs
+  }
+
+  def apply[A](as: A*): MyList[A] =
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+}
