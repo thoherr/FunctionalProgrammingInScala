f7d32a81b146b03ae3f428a4dc4c3651772f20ec
Thomas Herrmann <mail@thoherr.de>
2016-07-11 00:15:48 +0200

    Exercise 3.29

--- src/main/scala/ch03/MyTree.scala
+++ src/main/scala/ch03/MyTree.scala
@@ -32,5 +32,22 @@ object MyTree {
     case Branch(l, r) => Branch(map(l)(f), map(r)(f))
   }
 
+  // Exercise 3.29
+  def fold[A, B](t: MyTree[A])(f: A => B)(g: (B, B) => B) : B = t match {
+    case Leaf(l) => f(l)
+    case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g))
+  }
+
+  def sizeViaFold[A](t: MyTree[A]): Int =
+    fold(t)(a => 1)(1 + _ + _)
+
+  def maximumViaFold(t: MyTree[Int]): Int =
+    fold(t)(a => a)(_ max _)
+
+  def depthViaFold[A](t: MyTree[A]): Int =
+    fold(t)(a => 0)((d1,d2) => 1 + (d1 max d2))
+
+  def mapViaFold[A,B](t: MyTree[A])(f: A => B): MyTree[B] =
+    fold(t)(a => Leaf(f(a)): MyTree[B])(Branch(_,_))
 
 }
