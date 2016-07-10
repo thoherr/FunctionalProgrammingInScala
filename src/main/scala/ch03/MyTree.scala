85c0ffa4259b571b699de8d997e0e493b0be0e6e
Thomas Herrmann <mail@thoherr.de>
2016-07-10 23:35:32 +0200

    Exercises 3.26 and 3.27

--- src/main/scala/ch03/MyTree.scala
+++ src/main/scala/ch03/MyTree.scala
@@ -14,4 +14,17 @@ object MyTree {
     case Branch(l, r) => 1 + size(l) + size(r)
   }
 
+  // Exercise 3.26
+  def maximum(t: MyTree[Int]) : Int = t match {
+    case Leaf(n) => n
+    case Branch(l, r) => maximum(l) max maximum(r)
+  }
+
+  // Exercise 3.27
+  def depth[A](t: MyTree[A]) : Int = t match {
+    case Leaf(_) => 0
+    case Branch(l, r) => 1 + (depth(l) max depth(r))
+  }
+
+
 }
