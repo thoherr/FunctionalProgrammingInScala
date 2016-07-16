a46bf7b255fc42243e78eb2e57636446b53d3f3b
Thomas Herrmann <mail@thoherr.de>
2016-07-16 22:50:59 +0200

    Exercise 4.2

---
+++ src/main/scala/ch04/Ex_4_2.scala
@@ -0,0 +1,12 @@
+package ch04
+
+object Ex_4_2 {
+
+  def mean(xs: Seq[Double]): Option[Double] =
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+
+  def variance(xs: Seq[Double]): Option[Double] =
+    mean(xs) flatMap (m => mean(xs map (x => math.pow(x - m, 2))))
+
+}
