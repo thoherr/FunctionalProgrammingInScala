8913590fb6cb1995981a5ed25ff8fbc4fe3e3394
Thomas Herrmann <mail@thoherr.de>
2016-07-17 20:46:47 +0200

    Exercise 4.6

---
+++ src/main/scala/ch04/Either.scala
@@ -0,0 +1,30 @@
+package ch04
+
+sealed trait Either[+E, +A] {
+
+  // Exercise 4.6: map, flatMap, orElse, map2
+  def map[B](f: A => B): Either[E, B] = this match {
+    case Left(e) => Left(e)
+    case Right(a) => Right(f(a))
+  }
+
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+    case Left(e) => Left(e)
+    case Right(a) => f(a)
+  }
+
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+    case Left(_) => b
+    case Right(a) => Right(a)
+  }
+
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+    this flatMap (aa => b map (bb => f(aa, bb)))
+
+  // official solution for map2
+  def officialMap2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+    for { a <- this; b1 <- b } yield f(a,b1)
+
+}
+case class Left[+E](value: E) extends Either[E, Nothing]
+case class Right[+A](value: A) extends Either[Nothing, A]
